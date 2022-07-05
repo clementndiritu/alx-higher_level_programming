3da7b39572e6a891afd87f4afc58f54b9079a0cf
clementndiritu <clemndiritu@gmail.com>
2022-07-05 23:14:28 +0300

    t-8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,8 +5,3 @@
 def class_to_json(obj):
     """returns the dictionary description with simple data structure"""
     return obj.__dict__
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
